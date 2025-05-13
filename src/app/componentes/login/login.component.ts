9fa441c4515990bde2c5dcca75f5ea53b6747593
DESKTOP-FMGO2IU\saigo <ivaniglesias.can@gmail.com>
2025-05-13 05:01:48 +0200

    Pagina de inicio de sesion, faltan los metodos

--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, NgModule } from '@angular/core';
+import { FormsModule, NgModel } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  loginData = {
+    email: '',
+    password: '',
+    userType: 'user'
+  };
 
+  constructor(private router: Router) {}
+
+  onLogin() {
+    // Aquí realizarías la validación en la API o el backend
+    // Simulando un login exitoso según el tipo de usuario
+    if (this.loginData.userType === 'user') {
+      // Redirige a la vista del usuario
+      this.router.navigate(['/user-dashboard']);
+    } else if (this.loginData.userType === 'employee') {
+      // Redirige a la vista del empleado
+      this.router.navigate(['/employee-dashboard']);
+    }
+  }
 }
